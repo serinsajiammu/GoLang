094c79b61ff9d6f5876ab7b564bbedfdb435ab74
SERIN SAJI <44175778+serinsajiammu@users.noreply.github.com>
2020-03-21 11:21:27 +0530

    Add files via upload

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Serin"
+	var age = 20
+	var fl = 34.0
+	var bln = false
+	fmt.Println("I am\t" + name + "\tfrom Kerala")
+	fmt.Printf("My name is %s , from Kerala\n", name)
+	fmt.Printf("Age:%d\n", age)
+	fmt.Printf("%g\n", fl)
+	fmt.Printf("%t", bln)
+
+}
